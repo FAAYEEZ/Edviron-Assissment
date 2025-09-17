ed8e8ee94e6d64adef33b6c03a7e251effe60809
Faayeez20 <faayeez.211447.it@mhssce.ac.in>
2025-09-17 15:48:19 +0530

    health check problem

--- school-payment-backend/src/app.service.ts
+++ school-payment-backend/src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return '✅ Backend is running on Railway!';
+    return 'Backend is running 🚀';
   }
 }
